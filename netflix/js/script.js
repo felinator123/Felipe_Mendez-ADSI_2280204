7a51337590443f84893fb10827f4e06d99a36bcb
Andres <fmfelinator23@gmail.com>
2022-05-15 17:03:19 -0500

    modificando

---
+++ netflix/js/script.js
@@ -0,0 +1,22 @@
+
+
+
+/* CON FUNCION SEPARADAS  */
+
+    /* function intercambiarCiudad(){
+    document.getElementById("ImgPrincipal").src="img/ciudad_perdida.jpg";     
+    }
+    function intercambiarNorte(){
+        document.getElementById("ImgPrincipal").src="img/hombre_norte.jpg";
+    }
+    function intercambiarMorbius(){
+        document.getElementById("ImgPrincipal").src="img/morbius.jpg";
+    }
+    function intercambiarHeroe(){
+        document.getElementById("ImgPrincipal").src="img/un_heroe.jpg";
+    } */
+
+/* CON FUNCION JUNTA  */
+    function intercambiarImagen(ruta){
+        document.getElementById("ImgPrincipal").src= ruta;
+    }
